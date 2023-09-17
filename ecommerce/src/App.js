c0d5c3b7ba744ca202c6651c730ec274ebcf5f11
farshad <far5had@outlook.com>
2023-09-17 20:23:51 +0200

    fixed styles

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 import CategoriesList from "./components/CategoriesList";
 
 function App() {
-
   return (
     <div className="App">
+      <h1>Products</h1>
       <CategoriesList />
     </div>
   );
--- ecommerce/src/components/Product.js
+++ ecommerce/src/components/Product.js
@@ -1,8 +1,8 @@
 const Product = ({ imgURL, title }) => {
   return (
     <li className="product">
-      <img className="product-image" src={imgURL} alt={title} />
-      <h2 className="product-title">{title}</h2>
+      <img className="product-image" src={imgURL} alt={title} title={title} />
+      <p className="product-title">{title}</p>
     </li>
   );
 };
